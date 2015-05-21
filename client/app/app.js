736d2466b52552f9c8eff28de6c0496538a537ba
Sanghyeon-encored <yongdamsh@encoredtech.com>
2015-05-21 17:01:24 +0900

    first commit

---
+++ client/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('uplusApiApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
